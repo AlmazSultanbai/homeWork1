7d3b0c89778926f6af6dcdafa1299e5c16318201
Sultanbai Almaz <aldeco312@gmail.com>
2023-05-14 14:08:08 +0600

    myFisrtCommit  Yahoo this is my first real deal commit

--- homeWork1/ViewController.swift
+++ homeWork1/ViewController.swift
@@ -8,15 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var Go: UILabel!
+    
+//    @IBAction func showNextPage(_ sender: Any) {
+//        // print("button tapped")
+//if textField.text?.isEmpty ?? true {
+//
+//    textField.placeholder = " Fill the fied, please "
+//    textField.layer.borderColor = UIColor.red.cgColor
+//    textField.layer.borderWidth = 2
+//    }
+    
+    @IBOutlet weak var button: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        nameLabel.text = " best button ever "
+        nameLabel.textColor = .blue
+        nameLabel.font = .boldSystemFont(ofSize: 20)
     }
-
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        if textField.text?.isEmpty ?? true {
+            
+            textField.placeholder = " Fill the fied, please "
+            textField.layer.borderColor = UIColor.red.cgColor
+            textField.layer.borderWidth = 2
+            return false
+        }
+        return true
+    }
 }
-
